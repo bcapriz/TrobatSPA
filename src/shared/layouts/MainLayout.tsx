44a9d46f654e54a311152a9d6a843d1f1d56528b
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2026-06-09 22:45:40 -0300

    corrige estilo de scroll principal

--- src/shared/layouts/MainLayout.tsx
+++ src/shared/layouts/MainLayout.tsx
@@ -11,7 +11,7 @@ export function MainLayout() {
       <Sidebar />
       <div className="flex-1 flex flex-col overflow-hidden">
         <Topbar />
-        <main className="flex-1 overflow-y-auto p-6">
+        <main className="flex-1 overflow-y-auto p-6 custom-scrollbar">
           <Outlet />
         </main>
       </div>
